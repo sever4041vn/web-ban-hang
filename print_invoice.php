bc5ffc907d81961ddd87353492e4c405c324e17a
sever4041vn <severvn41@gmail.com>
2026-09-10 18:11:23 +0700

    Cập nhật hiển thị thông tin thanh toán trong hóa đơn: điều chỉnh điều kiện hiển thị tổng cộng cần thanh toán

--- print_invoice.php
+++ print_invoice.php
@@ -125,16 +125,27 @@ $items = $itemStmt->fetchAll();
                         <td colspan="5" class="p-0 text-end fw-bold h10">Tổng toa hàng:</td>
                         <td class="p-0 text-end fw-bold h10"><?= number_format($order['final_amount'], 0, ',', '.') ?></td>
                     </tr>
-                    <tr id="label_1" style="display: <?= $order['extra_value_1']!=0?"":"none"?>; text-align: right;">
+                    <tr id="label_1" style="display: <?= $order['extra_value_1'] != 0 ? "" : "none" ?>; text-align: right;">
                         <td colspan="5" class="p-0 text-end fw-bold h10"><?= $order['extra_label_1'] ?></td>
                         <td class="p-0 text-end fw-bold h10"><?= number_format($order['extra_value_1'], 0, ',', '.') ?></td>
                     </tr>
-                    <tr id="label_3" style="display: <?= $order['paid_amount']!=0?"":"none"?>; text-align: right;">
+                    <tr id="label_3" style="display: <?= $order['paid_amount'] != 0 ? "" : "none" ?>; text-align: right;">
                         <td colspan="5" class="p-0 text-end fw-bold h10"><?= $order['extra_label_3'] ?></td>
                         <td class="p-0 text-end fw-bold h10"><?= number_format($order['paid_amount'], 0, ',', '.') ?></td>
                     </tr>
-                    <tr>
-                        <td colspan="5" class="p-0 text-end fw-bold h10">Tổng cộng thanh toán:</td>
+                    <tr style="display: 
+                    <?php 
+                        if ($order["customer_id"]!=1&&$order["customer_id"]!=0) {
+                            echo "";
+                        }elseif($order["extra_value_1"]!=0){
+                            echo "";
+                        }elseif($order["paid_amount"]!=0){
+                            echo "";
+                        }else{
+                            echo "none";
+                        }
+                    ?>; text-align: right;">>
+                        <td colspan="5" class="p-0 text-end fw-bold h10">Tổng cộng cần thanh toán:</td>
                         <td class="p-0 text-end fw-bold h10"><?= number_format($order['debt_amount'], 0, ',', '.') ?></td>
                     </tr>
                     <tr class="no-print">
